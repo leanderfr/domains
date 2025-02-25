62eca128e1fabd8165318ae38c1fabf626aee818
leaderfr <leanderprogrammer@gmail.com>
2025-02-25 09:37:19 -0300

    Atl

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Cadastro de Domínios">
   <meta name="author" content="Leander Fruchting">
   <link rel="stylesheet" href="/assets/css/stylesheet.css?v=1">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/css/app.css'])
 
 </head>
 <body>
